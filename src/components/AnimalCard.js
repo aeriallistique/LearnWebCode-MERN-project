59f862a858c2b4a385dfce063bc554649a338bd9
aeriallistique <semeseriasu@gmail.com>
2023-06-20 08:21:07 +0100

    tried rednering class in AnimalCard conditionally

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -8,6 +8,7 @@ function AnimalCard(props) {
   const [draftSpecies, setDraftSpecies] = useState("")
 
   let margin= true;
+  
   async function submitHandler(e) {
     e.preventDefault()
     setIsEditing(false)
@@ -41,7 +42,7 @@ function AnimalCard(props) {
 
   return (
     
-    <div className={!margin && 'card mb-4' || 'card'}>
+    <div className={margin? 'card mb-4' : 'card'}>
       <div className="our-card-top">
         {isEditing && (
           <div className="our-custom-input">
